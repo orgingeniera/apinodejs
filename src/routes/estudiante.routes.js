e5e1663315f644cfa0db5cf16954338e0dc6a347
KristianCerpa <kristorres2010@gmail.com>
2025-05-13 13:53:15 -0500

    Obtener por id

--- src/routes/estudiante.routes.js
+++ src/routes/estudiante.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controlador = require('../controllers/estudiante.controller');
 
 router.get('/', controlador.listar);
+router.get('/:id', controlador.obtenerPorId);
 router.post('/', controlador.crear);
 router.delete('/:id', controlador.eliminar);
 router.put('/:id', controlador.actualizar);
