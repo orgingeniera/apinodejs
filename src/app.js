2ee145e9c2808e37bd1f797a729f8010efafb10d
orgingeniera <98349422+orgingeniera@users.noreply.github.com>
2025-05-13 16:58:19 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 require('dotenv').config();
 
 app.use(express.json());
-
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 // Rutas
 const estudianteRoutes = require('./routes/estudiante.routes');
 app.use('/api/estudiantes', estudianteRoutes);
